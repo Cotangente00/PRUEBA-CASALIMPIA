a8fd3a02e444e96ed1cd9dddbde9af65290bac63
Cotangente00 <julianavila458@gmail.com>
2024-12-26 11:51:31 -0500

    adelanto (intentando conectarlo a la base de datos)

--- src/main/java/com/test/productcontrol/model/Producto.java
+++ src/main/java/com/test/productcontrol/model/Producto.java
@@ -9,5 +9,31 @@ package com.test.productcontrol.model;
  * @author user
  */
 class Producto {
-    
+    private int id;
+    private String nombre;
+    private double precio;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
